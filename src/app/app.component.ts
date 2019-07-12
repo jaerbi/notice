32191752270a85aa1f8aa8ae5cecdeea480973a6
Yura Avramenko <jaerbi42@gmail.com>
2019-07-12 21:52:20 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+interface Notes {
+  title: string;
+  belt: string;
+}
+
+interface NotesId extends Notes {
+  id: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'jaerbiNotes';
+export class AppComponent implements OnInit {
+
+  notesColection: AngularFirestoreCollection<Notes>;
+  notes: any;
+
+  title: string;
+  belt: string;
+
+  notesDoc: AngularFirestoreDocument<Notes>;
+  note: Observable<Notes>;
+
+
+  constructor(private afs: AngularFirestore) {
+  }
+
+  copyToClipboard(text) {
+    var input = document.createElement('input');
+    input.setAttribute('value', text);
+    document.body.appendChild(input);
+    input.select();
+    var result = document.execCommand('copy');
+    document.body.removeChild(input)
+    return result;
+  }
+
+  ngOnInit() {
+    this.notesColection = this.afs.collection('notes');
+    this.notes = this.notesColection.snapshotChanges()
+      .pipe(map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Notes;
+          const id = a.payload.doc.id;
+          return {id, data};
+        });
+      }));
+  }
+
+  addNotes() {
+    this.belt = '#' + Math.random().toString().slice(2, 8);
+    this.afs.collection('notes').add({'title': this.title, 'belt': this.belt});
+    this.title = '';
+  }
+
+  getNotes(noteId) {
+    this.notesDoc = this.afs.doc('notes/' + noteId);
+    this.note = this.notesDoc.valueChanges();
+  }
+
+  deleteNote(noteId) {
+    this.afs.doc('notes/' + noteId).delete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+
+import { FormsModule } from "@angular/forms";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBMwqVBig_Kyj_h77wFUBplsHP_4zJK4oM",
+  authDomain: "jaerbinote.firebaseapp.com",
+  databaseURL: "https://jaerbinote.firebaseio.com",
+  projectId: "jaerbinote",
+  storageBucket: "jaerbinote.appspot.com",
+  messagingSenderId: "709142590499"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +23,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
