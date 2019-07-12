016b4e4195e374f5b08ca2050bdba2cc0afd8dfd
Yura Avramenko <jaerbi42@gmail.com>
2019-07-12 22:14:48 +0300

    delete: Delete olad hoisting from firebase and go on git hub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,12 @@ import {AngularFirestoreModule} from 'angularfire2/firestore';
 import { FormsModule } from "@angular/forms";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBMwqVBig_Kyj_h77wFUBplsHP_4zJK4oM",
-  authDomain: "jaerbinote.firebaseapp.com",
-  databaseURL: "https://jaerbinote.firebaseio.com",
-  projectId: "jaerbinote",
-  storageBucket: "jaerbinote.appspot.com",
-  messagingSenderId: "709142590499"
+  apiKey: "AIzaSyB8Nf9IAHYXWbQXcmUAd0sg6UTXt8S5HLI",
+  authDomain: "todo-jaerbi.firebaseapp.com",
+  databaseURL: "https://todo-jaerbi.firebaseio.com",
+  projectId: "todo-jaerbi",
+  storageBucket: "todo-jaerbi.appspot.com",
+  messagingSenderId: "189381062081"
 };
 
 @NgModule({
